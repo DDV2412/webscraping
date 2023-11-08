64264334df60e7c0f788a8fa369daaa0ca5c628a
Dian Dwi Vaputra <43642761+DDV2412@users.noreply.github.com>
2023-11-08 12:22:39 +0700

    Update submission.py

--- app/repository/submission.py
+++ app/repository/submission.py
@@ -62,7 +62,7 @@ class SubmissionRepo:
             "submissions": submissions,
             "aggrs": {
                 "journal": aggr_journal,
-                "duplicate: duplicate_titles,
+                "duplicate": duplicate_title_values,
             },
             "total": total_submissions,
             "current_page": page,
